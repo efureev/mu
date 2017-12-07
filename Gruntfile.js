969ae07fb5e10ae302f020bf9cbbb4f10afd7855
efureev <efureev@yandex.ru>
2017-12-07 15:34:08 +0500

    refactor test

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,5 +50,4 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.registerTask('default', ['concat']);
     grunt.registerTask('install', ['concat', 'uglify']);
-    grunt.registerTask('test', ['concat', 'uglify']);
 };
