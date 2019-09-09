4e9d862dacf5786e63d223026ff26004bfef0417
Евгений Фуреев <efureev@yandex.ru>
2019-09-09 14:44:32 +0500

    remove JEST from package.json

--- jest.config.js
+++ jest.config.js
@@ -162,7 +162,9 @@ module.exports = {
 
     // A map from regular expressions to paths to transformers
     // transform: null,
-
+    transform: {
+        '^.+\\.js?$': 'babel-jest',
+    },
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
     //   "/node_modules/"
