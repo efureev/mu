0841e14c4edcf2f39b6d4835c5dc7b333c838177
Евгений Фуреев <efureev@yandex.ru>
2019-12-22 00:05:35 +0500

    fix

--- jest.config.js
+++ jest.config.js
@@ -141,9 +141,9 @@ module.exports = {
     ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    testPathIgnorePatterns: [
+        '/tests/utils.js',
+    ],
 
     // The regexp pattern Jest uses to detect test files
     // testRegex: "",
