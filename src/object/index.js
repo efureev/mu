0a9904f0a60753be2599effa2cbddbaee45be7d8
Евгений Фуреев <efureev@yandex.ru>
2020-03-25 16:57:17 +0500

    fix

--- src/object/index.js
+++ src/object/index.js
@@ -5,10 +5,23 @@ import fromQueryString from './fromQueryString'
 import toQueryString from './toQueryString'
 import toQueryObjects from './toQueryObjects'
 import equals from './equals'
+import defaults from './defaults'
 import merge from './merge'
 import select from './select'
 import pick from './pick'
 import remove from './remove'
 import pathToObject from './pathToObject'
 
-export { getSize, fromQueryString, toQueryString, toQueryObjects, equals, merge, select, remove, pick, pathToObject }
+export {
+  getSize,
+  fromQueryString,
+  toQueryString,
+  toQueryObjects,
+  equals,
+  merge,
+  select,
+  remove,
+  pick,
+  pathToObject,
+  defaults,
+}
