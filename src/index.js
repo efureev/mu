c3fa4d5beb29b1363f67bdb476baadbd37c54fd0
Евгений Фуреев <efureev@yandex.ru>
2020-04-05 21:23:05 +0500

    fix path

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import clone from './core/clone'
 import tap from './core/tap'
 import toString from './to/toString'
 import equals from './core/equals'
+import forEach from './core/forEach'
 
-export {clone, toString, equals, tap}
+export {clone, toString, equals, tap, forEach}
