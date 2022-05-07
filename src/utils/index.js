e92586155733baa23c9155e40d2d4703a7177354
Jack <furegin@yandex.ru>
2022-05-07 12:44:42 +0500

    fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 import times from './times.js'
 import pregQuote from './pregQuote.js'
-import { b64ToUtf8, b64ToUtf8Safe, utf8ToB64, utf8Tob64Safe } from './base64'
+import { b64ToUtf8, b64ToUtf8Safe, utf8ToB64, utf8Tob64Safe } from './base64.js'
 
 export { times, utf8ToB64, b64ToUtf8, b64ToUtf8Safe, utf8Tob64Safe, pregQuote }
